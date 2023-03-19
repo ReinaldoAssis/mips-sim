77d71cef52657da1e7b02c3ec1079e688dd15578
Reinaldo Assis <randroi75@gmail.com>
2023-03-19 14:54:13 -0300

    feat: more tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -56,4 +56,40 @@ test("check assembler compiler instructions", () => {
 
   let ori = simservice.assemble("ori t5 t4 16");
   expect(ori).toContain("0x358d0010");
+
+  let slt = simservice.assemble("slt t5 t4 t3"); //*
+  expect(slt).toContain("0x018b682a");
+
+  let slti = simservice.assemble("slti t5 t4 16");
+  expect(slti).toContain("0x298d0010");
+
+  let sltiu = simservice.assemble("sltiu t5 t4 16");
+  expect(sltiu).toContain("0x2d8d0010");
+
+  let sltu = simservice.assemble("sltu t5 t4 t3");
+  expect(sltu).toContain("0x018b682b");
+
+  let sw = simservice.assemble("sw t0 16(t1)");
+  expect(sw).toContain("0xad280010");
+
+  let sub = simservice.assemble("sub t0 t1 t2");
+  expect(sub).toContain("0x012a4022");
+
+  let subu = simservice.assemble("subu t0 t1 t2");
+  expect(subu).toContain("0x012a4023");
+
+  let xor = simservice.assemble("xor t0 t1 t2");
+  expect(xor).toContain("0x012a4026");
+
+  let xori = simservice.assemble("xori t0 t1 16");
+  expect(xori).toContain("0x39280010");
+
+  let j = simservice.assemble("j 16");
+  expect(j).toContain("0x08000010");
+
+  let jal = simservice.assemble("jal 16");
+  expect(jal).toContain("0x0c000010");
+
+  let jr = simservice.assemble("jr t0");
+  expect(jr).toContain("0x01000008");
 });
