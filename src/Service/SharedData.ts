24641824929a0b376ed6868fa2ba4279a33f0a04
Reinaldo Assis <randroi75@gmail.com>
2023-06-18 19:53:18 -0300

    refac: comments

--- src/Service/SharedData.ts
+++ src/Service/SharedData.ts
@@ -11,12 +11,17 @@ export default class SharedData {
   //   public currentLine: number = 0;
   //   public onStep: Function = (n: number) => {};
 
+  // monaco editor instance
   public monacoEditor: any = null;
+  // monaco instance
   public monaco: any = null;
+  // Current address of the program
   public currentPc: number = 0x00400000;
+  // Start address of the program
   public PcStart: number = 0x00400000;
+  // Pure text code
   public code: string = "";
-
+  // Current model for simulation
   public currentProcessor: processor | null = null;
 
   public get currentStepLine(): number {
