bf7bdc5bba65045ac16775ec9bb0540ee806ba11
Reinaldo Assis <randroi75@gmail.com>
2023-11-28 22:29:35 -0300

    todo: credits

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -46,7 +46,7 @@ const LinkItems: Array<LinkItemProps> = [
   { name: "Simulator", icon: FiCodepen },
   { name: "Instruction Set", icon: FiBookOpen },
   // { name: "Examples", icon: FiCompass },
-  { name: "Credits", icon: FiStar },
+  // { name: "Credits", icon: FiStar },
   // { name: "Settings", icon: FiSettings },
 ];
 
@@ -167,84 +167,84 @@ const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
 interface MobileProps extends FlexProps {
   onOpen: () => void;
 }
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 4 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue("white", "gray.900")}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-      justifyContent={{ base: "space-between", md: "flex-end" }}
-      {...rest}
-    >
-      <IconButton
-        display={{ base: "flex", md: "none" }}
-        onClick={onOpen}
-        variant="outline"
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
+// const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+//   return (
+//     <Flex
+//       ml={{ base: 0, md: 60 }}
+//       px={{ base: 4, md: 4 }}
+//       height="20"
+//       alignItems="center"
+//       bg={useColorModeValue("white", "gray.900")}
+//       borderBottomWidth="1px"
+//       borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+//       justifyContent={{ base: "space-between", md: "flex-end" }}
+//       {...rest}
+//     >
+//       <IconButton
+//         display={{ base: "flex", md: "none" }}
+//         onClick={onOpen}
+//         variant="outline"
+//         aria-label="open menu"
+//         icon={<FiMenu />}
+//       />
 
-      <Text
-        display={{ base: "flex", md: "none" }}
-        fontSize="2xl"
-        fontFamily="monospace"
-        fontWeight="bold"
-      >
-        Logo
-      </Text>
+//       <Text
+//         display={{ base: "flex", md: "none" }}
+//         fontSize="2xl"
+//         fontFamily="monospace"
+//         fontWeight="bold"
+//       >
+//         Logo
+//       </Text>
 
-      <HStack spacing={{ base: "0", md: "6" }}>
-        <IconButton
-          size="lg"
-          variant="ghost"
-          aria-label="open menu"
-          icon={<FiBell />}
-        />
-        <Flex alignItems={"center"}>
-          <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: "none" }}
-            >
-              <HStack>
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://s2.glbimg.com/3auOxS3cG2mc_H5jFXDpxC7ol-w=/e.glbimg.com/og/ed/f/original/2016/09/12/dr-alan-turing-2956483.jpg"
-                  }
-                />
-                <VStack
-                  display={{ base: "none", md: "flex" }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2"
-                >
-                  <Text fontSize="sm">Guest</Text>
-                  <Text fontSize="xs" color="gray.600"></Text>
-                </VStack>
-                <Box display={{ base: "none", md: "flex" }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue("white", "gray.900")}
-              borderColor={useColorModeValue("gray.200", "gray.700")}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
-      </HStack>
-    </Flex>
-  );
-};
+//       <HStack spacing={{ base: "0", md: "6" }}>
+//         <IconButton
+//           size="lg"
+//           variant="ghost"
+//           aria-label="open menu"
+//           icon={<FiBell />}
+//         />
+//         <Flex alignItems={"center"}>
+//           <Menu>
+//             <MenuButton
+//               py={2}
+//               transition="all 0.3s"
+//               _focus={{ boxShadow: "none" }}
+//             >
+//               <HStack>
+//                 <Avatar
+//                   size={"sm"}
+//                   src={
+//                     "https://s2.glbimg.com/3auOxS3cG2mc_H5jFXDpxC7ol-w=/e.glbimg.com/og/ed/f/original/2016/09/12/dr-alan-turing-2956483.jpg"
+//                   }
+//                 />
+//                 <VStack
+//                   display={{ base: "none", md: "flex" }}
+//                   alignItems="flex-start"
+//                   spacing="1px"
+//                   ml="2"
+//                 >
+//                   <Text fontSize="sm">Guest</Text>
+//                   <Text fontSize="xs" color="gray.600"></Text>
+//                 </VStack>
+//                 <Box display={{ base: "none", md: "flex" }}>
+//                   <FiChevronDown />
+//                 </Box>
+//               </HStack>
+//             </MenuButton>
+//             <MenuList
+//               bg={useColorModeValue("white", "gray.900")}
+//               borderColor={useColorModeValue("gray.200", "gray.700")}
+//             >
+//               <MenuItem>Profile</MenuItem>
+//               <MenuItem>Settings</MenuItem>
+//               <MenuItem>Billing</MenuItem>
+//               <MenuDivider />
+//               <MenuItem>Sign out</MenuItem>
+//             </MenuList>
+//           </Menu>
+//         </Flex>
+//       </HStack>
+//     </Flex>
+//   );
+// };
