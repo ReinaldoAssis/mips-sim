7291f1614e3f2d71238c81a4987a48b105804bb9
Reinaldo Assis <randroi75@gmail.com>
2024-08-07 17:15:00 -0300

    feat: added option to download hex

--- src/Components/pages/Simulator View/HexView.tsx
+++ src/Components/pages/Simulator View/HexView.tsx
@@ -1,6 +1,7 @@
-import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Icon, Text } from "@chakra-ui/react";
 import React from "react";
 import { useEffect } from "react";
+import { IoIosDownload } from "react-icons/io";
 import { Instruction } from "../../../Service/SharedData";
 import SimulatorService from "../../../Service/SimulatorService";
 
@@ -19,30 +20,28 @@ export function HexDisplay({n,i}:{n:number,i:Instruction}){
 function HexView({program} : {program : Array<Instruction>}){
     let simservice = SimulatorService.getInstance();
 
-    // useEffect(()=>{
-    //     program.forEach(i=>{
-    //         console.log(i.memAddress,"0x"+i.machineCode.toString(16).padStart(8,"0"),i.humanCode);
-    //         let n = (i.machineCode & 0xff000000) >> 24
-    //         console.log(i.memAddress,"13x"+n.toString(16).padStart(2,"0"),i.humanCode)
-    //     })
-    // }, [program])
+    function downloadHex() 
+    {
+        let hexString = "";
+        program.forEach(i=>{
+            hexString += `0x${i.machineCode.toString(16)}\n`
+        });
+        const element = document.createElement("a");
+        const file = new Blob([hexString], {type: 'text/plain'});
+        element.href = URL.createObjectURL(file);
+        element.download = "hex.txt";
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+    }
 
-    return <Grid templateColumns='repeat(17, 50px)' gap={0} style={{marginTop:15}} >
+    return <>
+    <Button leftIcon={<IoIosDownload/>} onClick={() => downloadHex()}>Download Hex</Button>
+        <Grid templateColumns='repeat(17, 50px)' gap={0} style={{marginTop:15}} >
        {program.map((i,n) => {
          return <HexDisplay n={n} i={i} />
        })}
-       {/* {program.length == 0 ? <h3 style={{width:"100%"}}>First you must assemble the code.</h3> : <></>} */}
     </Grid>
+    </>
 }
 
 export default React.memo(HexView);
-
-{/* <div style={{display:"flex"}}>
-         <Text as="b">{n}</Text>
-         <Text as="b" marginLeft={10}>{i.memAddress}</Text>
-         <Text as="b" marginLeft={10}>0x{((i.machineCode & 0xff000000) >> 24 & 0xff).toString(16).padStart(2,"0")}</Text>
-         <Text as="b" marginLeft={10}>0x{((i.machineCode & 0x00ff0000) >> 16 & 0xff).toString(16).padStart(2,"0")}</Text>
-         <Text as="b" marginLeft={10}>0x{((i.machineCode & 0x0000ff00) >> 8 & 0xff).toString(16).padStart(2,"0")}</Text>
-         <Text as="b" marginLeft={10}>0x{((i.machineCode & 0x000000ff) & 0xff).toString(16).padStart(2,"0")}</Text>
-         <Text as="b" marginLeft={10}>{i.humanCode}</Text>
-     </div> */}
