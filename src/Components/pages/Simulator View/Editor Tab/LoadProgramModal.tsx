16640d26cafdd0817dd8b846ef99c80a8020f601
Reinaldo Assis <randroi75@gmail.com>
2024-10-01 07:45:28 -0300

    feat: added load from file

--- src/Components/pages/Simulator View/Editor Tab/LoadProgramModal.tsx
+++ src/Components/pages/Simulator View/Editor Tab/LoadProgramModal.tsx
@@ -36,6 +36,36 @@ export default function LoadProgramModal(props: {
     const share: SharedData = SharedData.instance;
     const [cachedPrograms, setCachedPrograms] = React.useState<string[]>([]);
 
+    function loadFromFile()
+    {
+        // Cria um elemento input invisível para selecionar o arquivo
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.accept = '.txt'; // Aceita apenas arquivos .txt
+
+      // Ao selecionar o arquivo
+      input.onchange = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        if (target.files && target.files.length > 0) {
+          const file = target.files[0];
+          const reader = new FileReader();
+
+          // Ao carregar o conteúdo do arquivo
+          reader.onload = (e) => {
+            const content = e.target?.result as string;
+            share.code = content
+            share.updateMonacoCode();
+            props.close()
+          };
+
+          reader.readAsText(file); // Lê o arquivo como texto
+        }
+      };
+
+      // Abre o diálogo para selecionar o arquivo
+      input.click();
+    }
+
     React.useEffect(() => {
       // if(cachedPrograms.length == 0){
         setCachedPrograms(share.getListOfCachedPrograms() as string[]);
@@ -78,7 +108,10 @@ export default function LoadProgramModal(props: {
             </ModalBody>
     
             <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={() => props.close()}>
+            <Button colorScheme="blue" mr={3} onClick={() => loadFromFile()}>
+                From File
+              </Button>
+              <Button colorScheme="red" mr={3} onClick={() => props.close()}>
                 Close
               </Button>
             </ModalFooter>
